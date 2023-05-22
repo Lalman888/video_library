fc0f309a1a4680879f733a91b9858ac10ea4d3a2
Lalman <chauhanlakshay074@gmail.com>
2023-05-22 19:18:50 +0530

    ADD: video fun

---
+++ app/watch/[videoId]/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useRouter,useSearchParams,usePathname } from "next/navigation";
+
+function WatchVideoPage() {
+  const pathname = usePathname();
+
+  return (
+    <div>
+      <video
+        src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/videos/${pathname.split('/')[2]}`}
+        width="800px"
+        height="auto"
+        controls
+        autoPlay
+        id="video-player"
+      />
+       
+    </div>
+  );
+}
+
+export default WatchVideoPage;
--- components/admin/UploadVideo.tsx
+++ components/admin/UploadVideo.tsx
@@ -1,4 +1,10 @@
-import React from "react"
+"use client"
+
+import React, { Fragment, useState } from "react"
+import { getVideo, updateVideo, uploadVideo } from "@/api"
+import { Video } from "@/types"
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios"
 
 import {
   Dialog,
@@ -8,6 +14,25 @@ import {
 } from "@/components/ui/dialog"
 
 const UploadVideo = () => {
+  const [progress, setProgress] = useState(0)
+
+  const config = {
+    onUploadProgress: (progressEvent: any) => {
+      const { loaded, total } = progressEvent
+      let percent = Math.floor((loaded * 100) / total)
+      console.log("percent: ", percent)
+      setProgress(percent)
+    },
+  }
+
+  const mutation = useMutation(uploadVideo)
+
+  const upload = (file: File[]) => {
+    const formData = new FormData()
+    formData.append("video", file[0])
+
+    mutation.mutate({ formData, config })
+  }
   return (
     <>
       <div className="flex h-screen w-full flex-col items-center justify-center bg-gray-100">
@@ -22,64 +47,39 @@ const UploadVideo = () => {
             </DialogTrigger>
             <DialogContent className="sm:max-w-[525px]">
               <DialogHeader>
+                <div className="mb-3 flex flex-col">
+                  <label
+                    className="mb-2 font-bold text-gray-800"
+                    htmlFor="video"
+                  >
+                    Video
+                  </label>
+                  <input
+                    className="rounded-md border border-gray-300 px-3 py-2"
+                    type="file"
+                    name="video"
+                    id="video"
+                    accept="video/mp4,video/x-m4v,video/*"
+                    multiple={false}
+                    // @ts-ignore
+                    onChange={(e) => upload(e.target.files as File[])}
+                    required
+                  />
+                </div>
 
-          <form
-            className="mt-4"
-            action="#"
-            method="POST"
-            encType="multipart/form-data"
-          >
-            <div className="mb-3 flex flex-col">
-              <label className="mb-2 font-bold text-gray-800" htmlFor="video">
-                Video
-              </label>
-              <input
-                className="rounded-md border border-gray-300 px-3 py-2"
-                type="file"
-                name="video"
-                id="video"
-                required
-              />
-            </div>
-            <div className="mb-3 flex flex-col">
-              <label className="mb-2 font-bold text-gray-800" htmlFor="title">
-                Title
-              </label>
-              <input
-                className="rounded-md border border-gray-300 px-3 py-2"
-                type="text"
-                name="title"
-                id="title"
-                placeholder="Enter title"
-                required
-              />
-            </div>
-            <div className="mb-3 flex flex-col">
-              <label
-                className="mb-2 font-bold text-gray-800"
-                htmlFor="description"
-              >
-                Description
-              </label>
-              <textarea
-                className="rounded-md border border-gray-300 px-3 py-2"
-                name="description"
-                id="description"
-                rows={3}
-                placeholder="Enter description"
-                required
-              ></textarea>
-            </div>
-            <div className="mt-4 flex items-center justify-center">
-              <button
-                className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-500 focus:bg-blue-700 focus:outline-none"
-                type="submit"
-              >
-                Upload
-              </button>
-            </div>
-          </form>
-          </DialogHeader>
+                {progress === 0 && (
+                  <>
+                    <div>{progress} %</div>
+                  </>
+                )}
+                {
+                  mutation.data && (
+                    <Fragment>
+                  <EditVideoForm videoId={mutation.data.videoId} />
+                    </Fragment>
+                  )
+                }
+              </DialogHeader>
             </DialogContent>
           </Dialog>
         </div>
@@ -89,3 +89,90 @@ const UploadVideo = () => {
 }
 
 export default UploadVideo
+
+export const EditVideoForm = ({ videoId }: { videoId: string }) => {
+  const [videoForm, setVideoForm] = useState({
+    title: "",
+    description: "",
+    published: false,
+  })
+  type input = Parameters<typeof updateVideo>["0"]
+
+  const mutation = useMutation<Video, AxiosError, input>(updateVideo, {
+    onSuccess: (data) => {
+      console.log("data: edit form ", data)
+    },
+  })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    mutation.mutate({
+      videoId,
+      ...videoForm,
+    })
+  }
+
+  return (
+    <>
+      <form className="mt-4" onSubmit={handleSubmit}>
+        <div className="mb-3 flex flex-col">
+          <label className="mb-2 font-bold text-gray-800" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="rounded-md border border-gray-300 px-3 py-2"
+            type="text"
+            name="title"
+            value={videoForm.title}
+            onChange={(e) =>
+              setVideoForm({ ...videoForm, title: e.target.value })
+            }
+            id="title"
+            placeholder="Enter title"
+            required
+          />
+        </div>
+        <div className="mb-3 flex flex-col">
+          <label className="mb-2 font-bold text-gray-800" htmlFor="description">
+            Description
+          </label>
+          <textarea
+            className="rounded-md border border-gray-300 px-3 py-2"
+            name="description"
+            id="description"
+            value={videoForm.description}
+            onChange={(e) =>
+              setVideoForm({ ...videoForm, description: e.target.value })
+            }
+            rows={3}
+            placeholder="Enter description"
+            required
+          ></textarea>
+        </div>
+        <div className="mb-3 flex flex-col">
+          <label className="mb-2 font-bold text-gray-800" htmlFor="published">
+            Published
+          </label>
+          <input
+            type="checkbox"
+            name="published"
+            id="published"
+            className="rounded-md border border-gray-300 px-3 py-2"
+            checked={videoForm.published}
+            onChange={(e) =>
+              setVideoForm({ ...videoForm, published: e.target.checked })
+            }
+          />
+        </div>
+        <div className="mt-4 flex items-center justify-center">
+          <button
+            className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-500 focus:bg-blue-700 focus:outline-none"
+            type="submit"
+          >
+            Upload
+          </button>
+        </div>
+      </form>
+    </>
+  )
+}
